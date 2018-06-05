39f47a7282a4a87f1c71e491672424cc37f9fa17
Naoya Taniguchi <naoyataniguchi@Naoya-no-MacBook-Air.local>
2018-06-06 00:13:21 +0900

    Initial Commit

---
+++ swiftcountup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftcountup
+//
+//  Created by Naoya Taniguchi on 2018/06/06.
+//  Copyright © 2018年 Naoya Taniguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
